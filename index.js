2d6227a859eaaa65f89f139688b927015e0038ec
Ansh rathod <67627096+Ansh-Rathod@users.noreply.github.com>
2021-11-11 23:19:48 -0800

    port dded

--- index.js
+++ index.js
@@ -17,4 +17,5 @@ app.use('/api/v1/post/comments', comment)
 app.use('/api/v1/search', search)
 app.use(errorHandler)
 
-app.listen(3000, console.log('Server running on port 3000'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log('Server running on port 3000'))
