8945661588659b5e11cabcaff8fbdd117a50fb27
Ansh rathod <67627096+Ansh-Rathod@users.noreply.github.com>
2021-11-17 03:05:13 -0800

    updated search query

--- routes/search/search.js
+++ routes/search/search.js
@@ -7,7 +7,7 @@ router.get(
 	'/',
 	asyncHandler(async (req, res, next) => {
 		var data = await pool.query(
-			`select * from appusers where name like '%${req.query.q}%'`
+			`select * from appusers where LOWER(name) like '%${req.query.q}%'`
 		)
 		res.status(200).json({
 			success: true,
