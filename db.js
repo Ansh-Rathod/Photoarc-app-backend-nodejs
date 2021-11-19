0dd7125d18f3c992719320ec6fc22f4cc32b4cb7
Ansh rathod <67627096+Ansh-Rathod@users.noreply.github.com>
2021-11-19 00:26:54 -0800

    fixing ssl suport

--- db.js
+++ db.js
@@ -3,6 +3,9 @@ import pg from 'pg'
 const Pool = pg.Pool
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 })
 
 export default pool
