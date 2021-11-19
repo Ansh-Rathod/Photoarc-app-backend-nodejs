6dac187f7edca7423619fea674a7d023e2862713
Ansh rathod <67627096+Ansh-Rathod@users.noreply.github.com>
2021-11-19 00:51:45 -0800

    complete in api

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ import search from './routes/search/search.js'
 import feed from './routes/feed/get-user-feed.js'
 import notifications from './routes/notifications/get-notifications.js'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import xss from 'xss-clean'
+import helmet from 'helmet'
 
 const app = express()
 dotenv.config({ path: './.env' })
-
+app.options('*', cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -24,5 +27,7 @@ app.use('/api/v1/feed', feed)
 app.use('/api/v1/notifications', notifications)
 app.use(errorHandler)
 
+app.use(helmet())
+app.use(xss())
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log('Server running on port 3000'))
